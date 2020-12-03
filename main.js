dbe9f2a4f2264501b9eccbd4bf4c9fc75375e0e4
Aliaksandr Danilin <al4icrux@gmail.com>
2020-12-03 12:55:40 +0300

    fix: after checked by a mentor;

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const showAmPm = true;
 
 // Show Time
 function showTime() {
-  let today = new Date();
+  const today = new Date();
   let  hour = today.getHours();
   let  min = today.getMinutes();
   let  sec = today.getSeconds();  
-  const amPm = hour >= 12 ? 'PM' : 'AM'; // Set AM or PM  
+  const timeFormat = hour >= 12 ? 'PM' : 'AM'; // Set AM or PM  
   hour = hour % 12 || 12; // 12hr Format  
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}  
-    ${showAmPm ? amPm : ''}`; // Output Time
+    ${showAmPm ? timeFormat : ''}`; // Output Time
   setTimeout(showTime, 1000);
 };
 
@@ -25,49 +25,40 @@ function addZero(n) {
 
 // Set Background and Greeting
 function setBgGreet() {
-  let today = new Date(),
-    hour = today.getHours();
-  if (hour < 12) {   
-    document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+  let today = new Date();
+  let hour = today.getHours();
+   if (hour < 12) {   
+    document.body.style.backgroundImage = `url('https://i.ibb.co/7vDLJFb/morning.jpg')`;
     greeting.textContent = 'Good Morning, ';
   } else if (hour < 18) {    
-    document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    document.body.style.backgroundImage = `url('https://i.ibb.co/3mThcXc/afternoon.jpg')`;
     greeting.textContent = 'Good Afternoon, ';
   } else {  
-    document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    document.body.style.backgroundImage = `url('https://i.ibb.co/924T2Wv/night.jpg')`;
     greeting.textContent = 'Good Evening, ';
     document.body.style.color = 'white';
   }
 };
 
-function getNameFocus() {
-  if (localStorage.getItem('name') === null){
-    name.textContent = '[Enter Name]';
-  } else {
-    name.textContent = localStorage.getItem('name');
-  } if (localStorage.getItem ('focus') === null) {
-    focus.textContent = '[Enter Focus]';
-  } else {
-    focus.textContent = localStorage.getItem('focus');
-  } 
+function getNameFocus() { 
+  name.textContent = localStorage.getItem('name') ? localStorage.getItem('name') :'[Enter Name]';
+  focus.textContent = localStorage.getItem ('focus') ? localStorage.getItem ('focus') : '[Enter Focus]'; 
 };
 
 // Set Name
 function setName(e) {
+  localStorage.setItem(e.target.id, e.target.innerText);
   if (e.type === 'keypress') {
-    // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem(e.target.id, e.target.innerText);
       if (e.target.id === name) {
         name.blur();
       } else {
         focus.blur();
       }
     }
-  } else {
-    localStorage.setItem(e.target.id, e.target.innerText);
   }
 };
+
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
