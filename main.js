f4798e90adf81f1bfb53216e5d975211374c3ac0
Ali Da <al4icrux@gmail.com>
2021-03-10 22:47:36 +0300

    refactor: apply prettier;

--- main.js
+++ main.js
@@ -1,61 +1,55 @@
-// DOM Elements
-const time = document.getElementById('time');
-const greeting = document.getElementById('greeting');
-const name = document.getElementById('name');
-const focus = document.getElementById('focus');
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const names = document.getElementById("name");
+const focus = document.getElementById("focus");
 const showAmPm = true;
-
 // Show Time
-function showTime() {
+const showTime = () => {
   const today = new Date();
   let hour = today.getHours();
   let min = today.getMinutes();
-  let sec = today.getSeconds();  
-  const timeFormat = hour >= 12 ? 'PM' : 'AM'; // Set AM or PM  
-  hour = hour % 12 || 12; // 12hr Format  
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? timeFormat : ''}`; // Output Time
+  let sec = today.getSeconds();
+  const timeFormat = hour >= 12 ? "PM" : "AM"; // Set AM or PM
+  hour = hour % 12 || 12; // 12hr Format
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} 
+  ${showAmPm ? timeFormat : ""}`; // Output Time
   setTimeout(showTime, 1000);
 };
-
 // Add Zeros
-let addZero = (n) => (parseInt(n, 10) < 10 ? '0' : '') + n;
-
+const addZero = (n) => (parseInt(n, 10) < 10 ? "0" : "") + n;
 // Set Background and Greeting
-let setBgGreet = () => {
+const setBgGreet = () => {
   let today = new Date();
   let hour = today.getHours();
-   if (hour < 12) {   
+  if (hour < 12) {
     document.body.style.backgroundImage = `url('https://i.ibb.co/7vDLJFb/morning.jpg')`;
-    greeting.textContent = 'Good Morning, ';
-  } else if (hour < 18) {    
+    greeting.textContent = "Good Morning, ";
+  } else if (hour < 18) {
     document.body.style.backgroundImage = `url('https://i.ibb.co/3mThcXc/afternoon.jpg')`;
-    greeting.textContent = 'Good Afternoon, ';
-  } else {  
+    greeting.textContent = "Good Afternoon, ";
+  } else {
     document.body.style.backgroundImage = `url('https://i.ibb.co/924T2Wv/night.jpg')`;
-    greeting.textContent = 'Good Evening, ';
-    document.body.style.color = 'white';
+    greeting.textContent = "Good Evening, ";
+    document.body.style.color = "white";
   }
 };
 
-let getNameFocus = () => { 
-  name.textContent = localStorage.getItem('name') ? localStorage.getItem('name') :'[Enter Name]';
-  focus.textContent = localStorage.getItem ('focus') ? localStorage.getItem ('focus') : '[Enter Focus]'; 
+const getNameFocus = () => {
+  names.textContent = localStorage.getItem("name") ? localStorage.getItem("name") : "[Enter Name]";
+  focus.textContent = localStorage.getItem("focus") ? localStorage.getItem("focus") : "[Enter Focus]";
 };
 
-// Set Name
-let setName = (e) => {
+const setName = (e) => {
   localStorage.setItem(e.target.id, e.target.innerText);
   if (e.which == 13 || e.keyCode == 13) {
-    e.target.id === name ? name.blur() : focus.blur();        
-  }  
+    e.target.id === names ? names.blur() : focus.blur();
+  }
 };
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setName);
-focus.addEventListener('blur', setName);
-
-// Run
+names.addEventListener("keypress", setName);
+names.addEventListener("blur", setName);
+focus.addEventListener("keypress", setName);
+focus.addEventListener("blur", setName);
 showTime();
 setBgGreet();
 getNameFocus();
